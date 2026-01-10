fb3f8bb9a461cfe79b5706775f87bdbe928c05dc
Alok Kumar Sharma <aloksharma1097@gmail.com>
2026-01-10 11:01:51 +0530

    file stream files added

---
+++ Revision/fs.js
@@ -0,0 +1,21 @@
+import fs from 'fs';
+
+const file = fs.writeFile("file.txt", "hello", "utf-8", (err, data) => {
+  if (err) {
+    console.log("Error creatig file");
+    return;
+  } else {
+    console.log(data)
+    console.log("File Created");
+  }
+
+  fs.readFile("file.txt", 'utf-8', (err, data) => {
+    if (err) {
+      console.log("Error reading file");
+      return;
+    }
+    else {
+      console.log(data);
+    }
+  });
+});
